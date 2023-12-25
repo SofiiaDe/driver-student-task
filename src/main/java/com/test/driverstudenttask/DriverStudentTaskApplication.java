80839364278c92404dbd34bc7ef953a7b3b73c97
SofiiaDe <sofiia.prydatko@gmail.com>
2023-12-25 12:51:46 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/test/driverstudenttask/DriverStudentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.test.driverstudenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverStudentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverStudentTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/driverstudenttask/DriverStudentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.driverstudenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverStudentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
