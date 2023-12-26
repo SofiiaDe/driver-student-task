ec015282cabcf0676bf2ea4882f944520387f485
SofiiaDe <sofiia.prydatko@gmail.com>
2023-12-26 09:06:47 +0200

    Implement student-grade functionality

--- students-app/src/main/java/com/task/students/Main.java
+++ students-app/src/main/java/com/task/students/Main.java
@@ -1,7 +1,45 @@
 package com.task.students;
 
+import com.task.students.utils.ConsoleOutputter;
+import com.task.students.utils.IOutputter;
+import com.task.students.utils.IStudentDataFormatter;
+import com.task.students.utils.SimpleStudentFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IOutputter outputter = new ConsoleOutputter();
+        IStudentDataFormatter studentDataFormatter = new SimpleStudentFormatter();
+        Department department = new Department(outputter, studentDataFormatter);
+
+        Student kateBerry = Student.builder()
+                .name("Kate")
+                .lastName("Berry")
+                .build();
+        department.addStudentGrade(kateBerry, 97);
+
+        Student tomSmith = Student.builder()
+                .name("Tom")
+                .lastName("Smith")
+                .build();
+        department.addStudentGrade(tomSmith, 95);
+
+        Student andrewLey = Student.builder()
+                .name("Andrew")
+                .lastName("Ley")
+                .build();
+        department.addStudentGrade(andrewLey, 81);
+        department.addStudentGrade(andrewLey, 92);
+
+        department.printAllStudentsGrades();
+
+        StudentHashCodeSample emmyHill = StudentHashCodeSample.builder()
+                .name("Emmy")
+                .lastName("Hill")
+                .build();
+        StudentHashCodeSample tonnyHill = StudentHashCodeSample.builder()
+                .name("Tonny")
+                .lastName("Hill")
+                .build();
+        System.out.println(emmyHill.hashCode() == tonnyHill.hashCode());
     }
 }
---
+++ students-app/src/main/java/com/task/students/utils/IStudentDataFormatter.java
@@ -0,0 +1,7 @@
+package com.task.students.utils;
+
+import com.task.students.Student;
+
+public interface IStudentDataFormatter {
+    String format(Student student);
+}
